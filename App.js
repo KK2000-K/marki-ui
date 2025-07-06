d64ed72583ea419fb2d5c9786538f0b1547b7c15
KK2000-K <krishnakapoor94@gmail.com>
2025-07-06 15:14:34 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Marki</h1>
+      <p>This is your AI-powered MR publishing platform.</p>
+    </div>
+  );
+}
+
+export default App;
